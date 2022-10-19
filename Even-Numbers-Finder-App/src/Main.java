56879d05209ccf62c66e19354c4ac0d7bc5fe7cd
Furkan Eralp Kahyaoğlu <82308276+kahyaoglou@users.noreply.github.com>
2022-10-19 14:57:21 +0300

    Update Main.java

--- Even-Numbers-Finder-App/src/Main.java
+++ Even-Numbers-Finder-App/src/Main.java
@@ -1,3 +1,4 @@
+/*Java döngüler ile kullanıcının girdiği sayıya kadar çift olan sayıları bulan program*/
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
@@ -15,3 +16,20 @@ public class Main {
 
     }
 }
+/*Java döngüler ile 0'dan girilen sayıya kadar olan sayılardan 3 ve 4'e tam bölünen sayıların ortalamasını hesaplayan program*/
+/*Scanner input = new Scanner(System.in);
+        int sayi, i, sayac = 0, toplam = 0, ortalama = 0;
+        System.out.print("Please enter the number: ");
+        sayi = input.nextInt();
+
+        for (i = 1 ; i <= sayi; i++)
+        {
+            if (i % 3 == 0 && i % 4 == 0){
+                sayac++;
+                System.out.print(i + ", ");
+                toplam += i;
+            }
+        }
+        ortalama = toplam / sayac;
+        System.out.println("Ortalama: " +ortalama);
+        */
