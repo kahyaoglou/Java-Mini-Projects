91b59bc94bb377e58388f2fc154033ecf5e9f055
Furkan Eralp Kahyaoğlu <82308276+kahyaoglou@users.noreply.github.com>
2022-10-20 01:38:05 +0300

    Add files via upload

---
+++ Exponent-Calculator/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int base, power, finalExponent = 1 ;
+
+        System.out.print("Please enter the base: ");
+        base = input.nextInt();
+        System.out.print("Please enter the power (exponent): ");
+        power = input.nextInt();
+
+        for (int i = 1; i <= power; i++)
+        {
+            finalExponent *= base;
+        }
+
+        System.out.print(base + " to the " + power + " power = " + finalExponent);
+    }
+}
