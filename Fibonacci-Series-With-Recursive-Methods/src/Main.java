8ee652bee2a20ee379b30cf8404dc30ab998469f
Furkan Eralp Kahyaoğlu <furkaneralpkahyaoglu@gmail.com>
2022-12-01 11:58:11 +0300

    Add files via upload

---
+++ Fibonacci-Series-With-Recursive-Methods/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+
+    public static int fib(int n){
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the number: ");
+        int number = input.nextInt();
+        System.out.println(fib(number));
+    }
+}
